222d958be26a79d32675c49d47c9e9824f999881
Jamal Bankole <jbankole@terpmail.umd.edu>
2024-12-19 18:26:51 -0500

    Hover feature

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
-        square.textContent = j;
         square.classList.add("item");
         container.appendChild(square);
     }
 }
 
+const items = document.querySelectorAll(".item");
+
+for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener('mouseover', (event) => {
+        event.target.style.borderColor = "red";
+    })
+}
